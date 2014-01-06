89741acb6c06ead6993ba1ce2f25ab38248908de
ricardo <ricardo@Bodhisatva>
2014-01-06 10:35:34 +0000

    File header update

--- src/main/java/com/ricardolorenzo/file/io/IOStreamUtils.java
+++ src/main/java/com/ricardolorenzo/file/io/IOStreamUtils.java
@@ -1,3 +1,19 @@
+/*
+ * IOStreamUtils class
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Ricardo Lorenzo <unshakablespirit@gmail.com>
+ */
 package com.ricardolorenzo.file.io;
 
 import java.io.ByteArrayOutputStream;
@@ -14,6 +30,11 @@ import java.util.zip.InflaterOutputStream;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * 
+ * @author Ricardo Lorenzo
+ * 
+ */
 public class IOStreamUtils {
     public final static int COMPRESSION_GZIP = 1;
     public final static int COMPRESSION_DEFLATE = 2;
--- src/main/java/com/ricardolorenzo/file/xml/XMLException.java
+++ src/main/java/com/ricardolorenzo/file/xml/XMLException.java
@@ -1,28 +1,39 @@
-package com.ricardolorenzo.file.xml;
 /*
- * XMLException.java
- *
- * Created on 1 de octubre de 2002, 10:09
+ * XMLException class
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Ricardo Lorenzo <unshakablespirit@gmail.com>
  */
-
-
+package com.ricardolorenzo.file.xml;
 
 /**
- *
+ * 
  * @author Ricardo Lorenzo
  */
 public class XMLException extends java.lang.Exception {
     private static final long serialVersionUID = 5248345389266919255L;
 
-	/**
+    /**
      * Creates a new instance of <code>XMLException</code> without detail message.
      */
     public XMLException() {
     }
-    
+
     /**
      * Constructs an instance of <code>XMLException</code> with the specified detail message.
-     * @param msg the detail message.
+     * 
+     * @param msg
+     *            the detail message.
      */
     public XMLException(String msg) {
         super(msg);
